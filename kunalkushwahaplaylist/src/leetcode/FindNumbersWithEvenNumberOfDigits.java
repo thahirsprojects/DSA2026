bf75bdedf554f83267ec62ca601e7fb87e64a183
thahirsprojects <thahirsprojects@gmail.com>
2025-12-22 14:41:02 +0530

    init

---
+++ kunalkushwahaplaylist/src/leetcode/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class FindNumbersWithEvenNumberOfDigits {
+    public static void main(String[] args) {
+        int[] nums = {34, 4, 2342, 435, 2345, 3, 3456};
+        System.out.println(findEvenDigits(nums));
+    }
+
+    private static int findEvenDigits(int[] nums) {
+        int count = 0;
+
+        for(int n : nums){
+            if(isEvenDigit(n)) count++;
+        }
+
+        return count;
+    }
+
+    private static boolean isEvenDigit(int n) {
+        int digitCount = 0;
+        while(n > 0){
+            n /= 10;
+            ++digitCount;
+        }
+
+        if(digitCount % 2 == 0) return true;
+
+        return false;
+    }
+}
