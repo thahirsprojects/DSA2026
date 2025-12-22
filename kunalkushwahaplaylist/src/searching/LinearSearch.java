a8a9458dcda36e27dd1906a903fb37d9dba68399
thahirsprojects <thahirsprojects@gmail.com>
2025-12-22 13:35:48 +0530

    init

---
+++ kunalkushwahaplaylist/src/searching/LinearSearch.java
@@ -0,0 +1,29 @@
+package searching;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {23, 34, 1, 35, 98, 54, 65};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the target : ");
+        int target = sc.nextInt();
+        int index = linearSearch(arr, target);
+        if(index!=-1){
+            System.out.println("Index of the element: " + index);
+        }
+        else{
+            System.out.println("Element not found !");
+        }
+    }
+
+    private static int linearSearch(int[] arr, int target) {
+        if(arr.length == 0) return -1;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target) return i;
+        }
+
+        return -1;
+    }
+}
