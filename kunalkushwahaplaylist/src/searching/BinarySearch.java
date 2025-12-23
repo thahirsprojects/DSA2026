cb37e547c64b48dc2b42f35f621fcbce534a9ac4
thahirsprojects <thahirsprojects@gmail.com>
2025-12-23 20:10:58 +0530

    Agnostic Binary Search

--- kunalkushwahaplaylist/src/searching/BinarySearch.java
+++ kunalkushwahaplaylist/src/searching/BinarySearch.java
@@ -12,6 +12,37 @@ public class BinarySearch {
         int target2 = 6;
         System.out.println(binarySearchForDesc(arr2, target2));
 
+        int[] arr3 = {76, 34, 23, 11, 6, 2, 1};
+        int target3 = 1;
+        System.out.println(binarySearchAgnostic(arr3, target3));
+
+    }
+
+    private static int binarySearchAgnostic(int[] arr3, int target3) {
+        int low = 0, high = arr3.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr3[mid] == target3) return mid;
+
+            if(arr3[low] < arr3[high]){
+                if (arr3[mid] < target3) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            else{
+                if (arr3[mid] > target3) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+
     }
 
     private static int binarySearch(int[] arr, int target) {
